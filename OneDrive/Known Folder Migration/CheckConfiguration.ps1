bc35824ef32beea3a22f346019541117a6721b85
thomas-ingle <54182378+thomas-ingle@users.noreply.github.com>
2023-02-20 21:32:37 -0500

    Added OneDrive Known Folder Migration scripts

---
+++ OneDrive/Known Folder Migration/CheckConfiguration.ps1
@@ -0,0 +1,8 @@
+﻿$downloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+
+#check if downloads path is set to subfolder in onedrive path
+if ($downloadsPath -like "*OneDrive - JCS Solutions LLC*") {
+    Write-Host "Downloads path is correctly configured in OneDrive"
+} else {
+    Write-Error "Downloads path is not correctly configured in OneDrive"
+}
